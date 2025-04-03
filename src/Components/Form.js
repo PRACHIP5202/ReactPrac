ba89f3b30fa1b18127c5b3831ae4dd799fbfcb12
Prachi P <prachi.p10305@gmail.com>
2025-04-03 07:47:52 +0530

    8 done

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,30 +1,40 @@
 import React, {useState} from 'react'
 
 export default function Form(props) {
-  const [text, setText] = useState('Enter text here');
+  const [text, setText] = useState('');
   // setText("I'm setting");
 
 
   const handeler = (event) => {
-    console.log("on change");
     setText(event.target.value);
   }
 
-  const handelupcli = (event) =>{
-    console.log("Uppercase was clicked");
+  const handelupcli = () =>{
     let newText =text.toUpperCase();
     setText(newText);
   }
 
+  const handelupclilow = () =>{
+    let newText =text.toLowerCase();
+    setText(newText);
+  }
 
   return (
     <div>
-      
+      <h1>{props.heading}</h1>
 <div className="mb-3">
   
-  <h1>{props.heading}</h1>
   <textarea className="form-control" value  ={text} onChange={handeler} id="mybox " rows="10"></textarea>
-  <button type="button" onClick={handelupcli} class="btn btn-primary">submit</button>
+  <button type="button" onClick={handelupcli} className="btn btn-primary mx-2">uppercase</button>
+  <button type="button" onClick={handelupclilow} className="btn btn-primary mx-2">lowercase</button>
+</div>
+
+<div className="mb-3">
+  <p>Need count??</p>
+  <p>{text.split(" ").length} and {text.length}  </p>
+  <p>{0.008* text.split(" ").length} min is requirred to read</p>
+  <h3>Preview</h3>
+  <p>{text}</p>
 </div>
     </div>
   )
